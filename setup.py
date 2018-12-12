60291ba37ab2853b4fa9bbae3b136c1c56697ec8
Radu Boncea <radu@rotld.ro>
2018-12-12 21:43:05 +0200

    small update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A wrapper API of ANAF web services",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/agilegeeks/pyAnaf.git",
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
